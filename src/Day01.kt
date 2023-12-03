5ef35f9c80fcfc4af654e18a79c67e6892c85992
ketkev <kdjoosten@gmail.com>
2023-12-03 20:12:01 +0100

    Day 01

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,15 +1,41 @@
 fun main() {
     fun part1(input: List<String>): Int {
-        return input.size
+        return input.sumOf { line ->
+            val digits = line.filter { it.isDigit() }
+
+            digits.first().toString().plus(digits.last()).toInt()
+        }
     }
 
     fun part2(input: List<String>): Int {
-        return input.size
+        val letters = mapOf(
+            "one" to "o1e",
+            "two" to "t2o",
+            "three" to "t3e",
+            "four" to "f4r",
+            "five" to "f5e",
+            "six" to "s6x",
+            "seven" to "s7n",
+            "eight" to "e8t",
+            "nine" to "n9e"
+        )
+
+        return input.sumOf { line ->
+            val replacedLine = letters.entries.fold(line) { acc, (target, replacement) ->
+                acc.replace(target, replacement)
+            }
+
+            val digits = replacedLine.filter { it.isDigit() }
+
+            digits.first().toString().plus(digits.last()).toInt()
+        }
     }
 
-    // test if implementation meets criteria from the description, like:
-    val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
+    val part1TestInput = readInput("Day01_test_part1")
+    check(part1(part1TestInput) == 142)
+
+    val part2TestInput = readInput("Day01_test_part2")
+    check(part2(part2TestInput) == 281)
 
     val input = readInput("Day01")
     part1(input).println()
